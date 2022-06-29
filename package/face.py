dfba9dbb9e60de5c1d48c1bb18569eb591346e49
Zac Scheiwe <=>
2022-06-29 21:31:26 +1200

    Refactored Face

--- package/face.py
+++ package/face.py
@@ -1,29 +1,51 @@
 from termcolor import colored  # type: ignore
 import os
 
-class Face:
+class Color:
+    """A color of a face on a Rubik's cube."""
 
-    os.system('color')
-    color_names = {"B": "blue", "R": "red", "Y": "yellow",
-                    "W": "white", "O": "orange", "G": "green"}
-
-    def __init__(self, color: str, coord: int):
-        if color not in Face.color_names.keys():
-            raise ValueError("Not a valid color (B,R,Y,W,O,G)")
+    VALID_COLORS: list[str] = ["blue", "red", "yellow", "white", "orange", "green"]
+    
+    @staticmethod
+    def get_valid_color(color: str) -> str:
+        color_len: int = len(color)
+        for valid_color in Color.VALID_COLORS:
+            if color.lower() == valid_color[0:color_len].lower():
+                return valid_color
         
-        self._color: str = color
-        self._coord: int = coord
+        # Else not a valid color:
+        raise ValueError("Not a valid color (B,R,Y,W,O,G)")
 
+    def __init__(self, color: str):
+        self._color = Color.get_valid_color(color)
+    
+    @property
+    def color_name(self):
+        return self._color
+    
     @property
     def color(self):
-        return self._color
+        return self.color_name[0].upper()
+
+    def __str__(self):
+        return colored("██", self.color_name)
+
+
+class Face(Color):
+    """A face of one piece on a Rubik's cube."""
+
+    os.system('color')
+
+    def __init__(self, color: str, coord: int):
+        Color.__init__(self, color)
+        self._coord: int = coord
 
     @property
     def coord(self):
         return self._coord
 
-    def __str__(self):
-        return colored(self._color, Face.color_names[self._color])
+    def __repr__(self):
+        return colored(f"({self.coord})", self.color_name)
 
     def switch(self):
         self._coord = -self._coord
--- tests/test_face.py
+++ tests/test_face.py
@@ -1,17 +1,23 @@
 import pytest
-from package.face import *
+from termcolor import colored  # type: ignore
+from package.face import Color, Face
 
 def test_invalid_color():
-    with pytest.raises(ValueError) as e_info:
-        my_face = Face("A", 1)
+    with pytest.raises(ValueError) as _:
+        Color("lue")
+
+def test_color_printout(capsys):
+    my_face = Face("yell", 3)
+    print(my_face)
+    captured = capsys.readouterr()
+    assert captured.out == colored("██", "yellow")+"\n"
 
 def test_face_properties():
-    my_face = Face("W", 2)
+    my_face = Face("wh", 2)
     assert my_face.color == "W"
     assert my_face.coord == 2
 
-def test_face_printout(capsys):
-    my_face = Face("Y", 3)
-    print(my_face)
-    captured = capsys.readouterr()
-    assert captured.out == colored("Y", "yellow")+"\n"
+def test_face_switch():
+    my_face = Face("re", 1)
+    my_face.switch()
+    assert my_face.coord == -1
